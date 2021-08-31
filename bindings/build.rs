e20981da69b1156a9cee4fae3cd86cb28998f9d4
Fu Wang <wangfu91@hotmail.com>
2021-08-31 18:38:23 +0800

    Save in progress work

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,5 +1,27 @@
 fn main() {
     windows::build! {
-        
+        Windows::Win32::Storage::FileSystem::{
+            CreateFileW,
+            USN_JOURNAL_DATA_V0,
+            READ_USN_JOURNAL_DATA_V0,
+            FILE_ACCESS_FLAGS,
+            FILE_SHARE_MODE,
+            FILE_CREATION_DISPOSITION,
+            FILE_FLAGS_AND_ATTRIBUTES
+
+        },
+
+        Windows::Win32::System::SystemServices:: {
+            DeviceIoControl,
+            FSCTL_QUERY_USN_JOURNAL,
+        },
+
+        Windows::Win32::Security::{
+            SECURITY_ATTRIBUTES,
+        },
+
+        Windows::Win32::Foundation::{
+            HANDLE,
+        },
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use bindings::Windows::Win32::{Foundation::{HANDLE,}, Security::{SECURITY_ATTRIBUTES,}, Storage::FileSystem::{CreateFileW, FILE_ACCESS_FLAGS, FILE_CREATION_DISPOSITION, FILE_FLAGS_AND_ATTRIBUTES, FILE_GENERIC_READ, FILE_SHARE_MODE, FILE_SHARE_READ, FILE_SHARE_WRITE, READ_USN_JOURNAL_DATA_V0, USN_JOURNAL_DATA_V0}, System::SystemServices::{DeviceIoControl, FSCTL_QUERY_USN_JOURNAL}};
+
+
 fn main() {
     println!("Hello, world!");
+    let handle = CreateFileW(
+        "\\.\E:", 
+    FILE_GENERIC_READ , 
+    FILE_SHARE_READ|FILE_SHARE_WRITE, 
+    lpsecurityattributes, dwcreationdisposition, dwflagsandattributes, htemplatefile)
 }
